bd4d444be35ea6501cd73502bd87966d80a582b5
96yrlee <96yrlee@gmail.com>
2020-11-11 23:03:34 -0500

    Add files via upload

---
+++ app/build/generated/source/buildConfig/androidTest/debug/com/example/covid19appretrotest/test/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.covid19appretrotest.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.covid19infoandroidapp.test";
+  public static final String BUILD_TYPE = "debug";
+}
